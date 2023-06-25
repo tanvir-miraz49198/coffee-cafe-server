44a34389f23fb2b527769a7a68d3cbd4ac5cf432
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-06-25 09:20:23 +0600

    post data in server

--- index.js
+++ index.js
@@ -28,14 +28,28 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+
+
+
+
+   app.post('/coffee', async(req, res) => {
+    const newCoffee = req.body;
+    console.log(newCoffee)
+    const result = await coffeeCollection.insertOne(newCoffee);
+    res.send(result)
+   })
+
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
