fab8ddb4843831077db03a9882fb45e264b8d9f0
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-06-26 11:59:31 +0600

    coffee updated from server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -35,6 +35,20 @@ async function run() {
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
 
+    app.get('/coffee', async(req,res) => {
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+
+
+    app.get('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query);
+      res.send(result)
+    })
 
 
 
@@ -43,7 +57,43 @@ async function run() {
     console.log(newCoffee)
     const result = await coffeeCollection.insertOne(newCoffee);
     res.send(result)
+
    })
+
+
+
+   app.put('/coffee/:id', async(req, res) => {
+    const id = req.params.id;
+    const filter = {_id: new ObjectId(id)}
+    const options = { upsert: true };
+    const updatedCoffee = req.body;
+    const coffee = {
+      $set: {
+        name: updatedCoffee.name, 
+        quantity: updatedCoffee.quantity, 
+        supplier: updatedCoffee.supplier,
+        taste: updatedCoffee.taste, 
+        category: updatedCoffee.category, 
+        details: updatedCoffee.details, 
+        photoUrl: updatedCoffee.photoUrl
+      }
+    }
+    const result = await coffeeCollection.updateOne(filter, coffee, options)
+    res.send(result)
+
+   })
+
+
+
+   app.delete('/coffee/:id', async(req , res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await coffeeCollection.deleteOne(query);
+    res.send(result)
+
+   })
+
+
 
     
     await client.db("admin").command({ ping: 1 });
